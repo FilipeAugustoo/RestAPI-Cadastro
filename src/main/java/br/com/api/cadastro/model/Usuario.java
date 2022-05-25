4c1b5c4a9d7a39b650a215db8a1d4ed4eea679a9
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-05-24 22:03:04 -0300

    Commit

--- src/main/java/br/com/api/cadastro/model/Usuario.java
+++ src/main/java/br/com/api/cadastro/model/Usuario.java
@@ -2,28 +2,36 @@ package br.com.api.cadastro.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Usuario {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
     private Integer id;
 
     @NotBlank
     @Pattern(regexp = "^[a-z0-9_]+$")
+    @Size(max = 25)
     private String usuario;
     
     @NotBlank
+    @Size(max = 25)
     private String senha;
 
     
