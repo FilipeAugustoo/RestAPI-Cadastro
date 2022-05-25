ac55bed02ce0ec46138fe335079a560405969fa0
Filipe Augusto <filipeaugusto2130@gmail.com>
2022-05-25 18:54:15 -0300

    Commit

--- src/main/java/br/com/api/cadastro/model/Usuario.java
+++ src/main/java/br/com/api/cadastro/model/Usuario.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -34,7 +35,7 @@ public class Usuario {
     @Size(max = 25)
     private String senha;
 
-    @NotBlank
+    @NotNull
     private Integer saldo;
 
     
